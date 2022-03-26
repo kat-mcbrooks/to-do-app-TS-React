ec6c2f68f9900249ef2328993e6f490ea9985f2c
kat <katharinebrooks@hotmail.co.uk>
2022-03-26 22:05:57 +0000

    edit, delete, done icons added to single todos. basic styling. useState implemented for edit mode and editing todo input

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { Todo } from "../model";
+import { GrEdit } from "react-icons/gr";
+import { MdDone, MdDeleteForever } from "react-icons/md";
+import "./styles.css";
+import TodoList from "./TodoList";
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-const SingleTodo = () => {
-  return <div>SingleTodo</div>;
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo); // this is what's in the edit input box so setEditTodo will not actually change the todo text, only the editTodo text
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const handleEdit
+  return (
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id}) >
+      {edit ? (
+        <input
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos__single--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <GrEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <MdDeleteForever />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
 interface Props {
   todos: Todo[];
@@ -10,7 +11,12 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
     <div className="todos">
       {todos.map((todo) => (
-        <li>{todo.todo}</li>
+        <SingleTodo
+          todo={todo}
+          key={todo.id}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
